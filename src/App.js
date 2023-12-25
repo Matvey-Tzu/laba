e41a6b9c5d2f2450430a1af6c661009277d37eec
Matvey <deardeerxxx2@gmail.com>
2023-12-25 20:20:36 +0300

    new feach

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Казакова Матвея Михайловича 3";
+  const greeting = "Это тестовый проект Казакова Матвея Михайловича 4";
 
   return (
     <div className="App">
